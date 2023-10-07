0c156eab94b78e67dfb1c2d20ebd5263221396bc
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-10-07 15:52:32 +0900

    Create #1158.c

---
+++ #1158.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    int a;
+    scanf("%d",&a);
+    if ((40>=a && 30<=a)||(70>=a && 60<=a))
+        printf("win");
+    else 
+        printf("lose");
+    
+      
+    
+}
