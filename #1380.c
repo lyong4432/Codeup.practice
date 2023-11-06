558727fc190424000f664ed67003240d7e1166e8
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-07 07:46:27 +0900

    Create #1380.c

---
+++ #1380.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=6;i++)
+    {    for(int j=6;j>=1;j--)
+        {
+            if(i+j==n) printf("%d %d\n",i,j);
+
+        }
+    }
+
+}
